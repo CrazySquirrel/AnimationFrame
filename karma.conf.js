0e240fdef3e8ca42deb744cc61a7d076f1d23a00
Sergey Yastrebov <yastreb-serdg@mail.ru>
2017-01-27 18:40:28 +0300

    Trevis auto tests

--- karma.conf.js
+++ karma.conf.js
@@ -3,47 +3,6 @@
 var webpackConfig = require('./webpack.karma.config');
 webpackConfig.entry = {};
 
-var _package = require('./package.json');
-var devices = require('./vendors/BrowserStackLocal/Devices/Devices.json');
-
-/**
- * Tested devices
- */
-let arrDevices = {};
-/**
- * Filling the set of test devices from the configuration file
- */
-for (let x in devices) {
-    if (devices.hasOwnProperty(x)) {
-        let device = {
-            base: 'BrowserStack'
-        };
-        if (x == "OS X" || x == "Windows") {
-            device.resolution = '1280x1024';
-            device.os = x;
-            for (let y in devices[x]) {
-                if (devices[x].hasOwnProperty(y)) {
-                    device.os_version = y;
-                    for (let z in devices[x][y]) {
-                        if (devices[x][y].hasOwnProperty(z)) {
-                            device.browser = z;
-                            for (let i of devices[x][y][z]) {
-                                device.browser_version = i;
-                                arrDevices[
-                                device.os + " " +
-                                device.os_version + " " +
-                                device.browser + " " +
-                                device.browser_version
-                                    ] = Object.assign({}, device);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
 module.exports = function (config) {
     config.set({
         basePath: '',
@@ -88,16 +47,14 @@ module.exports = function (config) {
                 chunks: false
             }
         },
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
         browsers: [
-            'Chrome',
-            'ChromeCanary',
-            'Chromium',
-            'Firefox',
-            'FirefoxDeveloper',
-            'Safari',
-            'PhantomJS',
-            'OperaClassic',
-            'Opera'
+            'Chrome_travis_ci'
         ]
     })
 };
