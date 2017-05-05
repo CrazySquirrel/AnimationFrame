9968cfbd51fadc3ec14214e60ef5a1525d150bc7
Sergey Yastrebov <yastreb-serdg@mail.ru>
2017-05-05 18:01:45 +0300

    fix(Package): Package js bug fix

---
+++ package.d.ts
@@ -0,0 +1,5 @@
+declare const PACKAGE: {
+    "name": string;
+    "version": string;
+};
+export default PACKAGE;
---
+++ package.ts
@@ -0,0 +1,4 @@
+
+const PACKAGE = {"name":"AnimationFrame","version":"1.0.48"};
+export default PACKAGE;
+module.exports = PACKAGE;
